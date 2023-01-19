e678471cdbc53741205d3c93fdd50a43501ced68
zfxd <zhang_weiyao@yahoo.com.sg>
2023-01-18 16:29:41 -0800

    lab2 D:

---
+++ Vincent/Lab2/app/src/test/java/edu/ucsd/cse110/lab2/ExampleUnitTest.java
@@ -0,0 +1,19 @@
+package edu.ucsd.cse110.lab2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import androidx.test.core.app.ActivityScenario;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void should_always_pass() {
+        assertEquals(2, 1 + 1);
+    }
+}
